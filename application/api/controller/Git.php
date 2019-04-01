3a9c0ef23c32fa3f3b961f9e710d6bb5b18a85ce
shengli <363912809@qq.com>
2019-04-01 11:16:17 +0800

    git

--- application/api/controller/Git.php
+++ application/api/controller/Git.php
@@ -14,7 +14,7 @@ class Git extends Base {
 	}
 
 	public function pull(){
-		$output = shell_exec("cd /home/www/guodian; git pull 2<&1");
+		$output = shell_exec("cd /phpstudy/www/TP; git pull 2<&1");
         echo "<pre>$output</pre>";
 
 	}
