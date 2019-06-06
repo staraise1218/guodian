8fcb97a7049e350f1bc1e4267fb7d72756b68472
guofeng-mi <guofeng_mi@sina.com>
2019-06-06 09:51:30 +0800

    asdfafa

--- page/src/js/commodity.js
+++ page/src/js/commodity.js
@@ -23,13 +23,13 @@ let user_id = myUsetInfo.user_id;
  *          goodsid     //   theRequest
  * =================================================
  */
-urlinfo=window.location.href; //获取当前页面的url
-len=urlinfo.length;//获取url的长度
-offset=urlinfo.indexOf("?");//设置参数字符串开始的位置
-newsidinfo=urlinfo.substr(offset,len)//取出参数字符串 这里会获得类似“id=1”这样的字符串
-newsids=newsidinfo.split("=");//对获得的参数字符串按照“=”进行分割
-newsid=newsids[1];//得到参数值
-goods_id = newsid;
+// urlinfo=window.location.href; //获取当前页面的url
+// len=urlinfo.length;//获取url的长度
+// offset=urlinfo.indexOf("?");//设置参数字符串开始的位置
+// newsidinfo=urlinfo.substr(offset,len)//取出参数字符串 这里会获得类似“id=1”这样的字符串
+// newsids=newsidinfo.split("=");//对获得的参数字符串按照“=”进行分割
+// newsid=newsids[1];//得到参数值
+goods_id = getParam('goods_id');
 
 
 /**
