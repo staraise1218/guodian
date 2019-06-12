3ddb1e81db4dbb6eca26289f87d097238b92e1c7
guofeng-mi <guofeng_mi@sina.com>
2019-06-12 18:37:56 +0800

    aksjnfkfjnk

--- page/src/js/orderReturn.js
+++ page/src/js/orderReturn.js
@@ -1,13 +1,48 @@
 /**
  * @pay_status          【1 待审核  3 已退款  4 拒绝退款】
+ * @user_id
+ * @page
  */
 
-
-
+let myUsetInfo = localStorage.getItem('USERINFO');
+myUsetInfo = JSON.parse(myUsetInfo);
+console.log(myUsetInfo)
+let user_id = myUsetInfo.user_id;
 let pay_status = getParam('pay_status');
+let page = 1;
 
 
 
+
+
+getList()
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getList() {
+    $.ajax({
+        type: 'post',
+        url: GlobalHost + '/Api/order/order_list',
+        data: {
+            user_id: user_id,
+            type: "REFUND",
+            page: page
+        },
+        success: function(res) {
+            console.log(res)
+        }
+    })
+}
 
 
 
--- page/src/js/shoppingBag.js
+++ page/src/js/shoppingBag.js
@@ -1,4 +1,4 @@
-// alert('加载购物车')
+alert(window.location.href)
 /**定义数据
  * @user_id         【用户id】
  * @goodsList       【购物车商品数据】
