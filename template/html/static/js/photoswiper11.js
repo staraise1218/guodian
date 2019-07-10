8de648d779e01ac54c5140b55d42d5a572bcbc75
guofeng-mi <guofeng_mi@sina.com>
2019-07-10 19:50:31 +0800

    asfnka

--- template/html/static/js/photoswiper11.js
+++ template/html/static/js/photoswiper11.js
@@ -27,7 +27,7 @@
                         // create slide object
                         item = {
                             src: linkEl.getAttribute('href'),
-                            w: parseInt(size[0], 20),
+                            w: parseInt(size[0], 10),
                             h: parseInt(size[1], 10)
                         };
 
@@ -199,9 +199,9 @@
             initPhotoSwipeFromDOM('.my-gallery');
             
 
-            auto_data_size();
-                // window.onload = function() {
-                // };
+                window.onload = function() {
+                    auto_data_size();
+                };
 
                 function auto_data_size() {
                     var imgss = $("figure img");
@@ -209,7 +209,7 @@
                         var imgs = new Image();
                         imgs.src = $(this).attr("src");
                         var w = imgs.width,
-                        h = imgs.height;
+                            h = imgs.height;
                         $(this).parent("a").attr("data-size", "").attr("data-size", w + "x" + h);
                     })
                 }
