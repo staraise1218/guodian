4584cdb8a9b6f193d8dd4d0d54aa3809b0480adf
shengli <363912809@qq.com>
2019-07-22 12:00:23 +0800

    bug

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -119,7 +119,7 @@ class Goods extends Base {
 		$goodslist = Db::name('goods')
 			->where($where)
 			->order('sort asc, goods_id desc')
-			->field('goods_id, goods_name, store_count, original_img, shop_price')
+			->field('goods_id, goods_name, store_count, original_img, shop_price, reserved')
 			->limit($num)
 			->select();
 
