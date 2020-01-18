94ae704bb6476f7b6b0d4da31cb168e4876a8954
shengli <363912809@qq.com>
2020-01-18 21:30:12 +0800

    bug

--- application/mobile/controller/Goods.php
+++ application/mobile/controller/Goods.php
@@ -634,7 +634,7 @@ class Goods extends Base {
     public function brandList(){
         $count = db('brand')->group('name')->count();   //总页数
         $page = new Page($count, 4, '', 3);
-        $brand = db('brand')->order('sort asc')->group('name')->limit($page->firstRow . ',' . $page->listRows)->select();
+        $brand = db('brand')->order('store_count !=0 desc, sort asc, goods_id desc')->group('name')->limit($page->firstRow . ',' . $page->listRows)->select();
         $goods = db('goods')->group('brand_id')->field('brand_id,goods_id')->select();
         $this->assign('page',$page);
         $this->assign('goods',array_column($goods,'goods_id','brand_id'));
