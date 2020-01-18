22516abb940d28a5911796ad40a06c2c30b00547
shengli <363912809@qq.com>
2020-01-18 21:39:38 +0800

    brandlist

--- application/mobile/controller/Goods.php
+++ application/mobile/controller/Goods.php
@@ -635,7 +635,7 @@ class Goods extends Base {
         $count = db('brand')->group('name')->count();   //总页数
         $page = new Page($count, 4, '', 3);
         $brand = db('brand')->order('sort asc')->group('name')->limit($page->firstRow . ',' . $page->listRows)->select();
-        $goods = db('goods')->order('store_count !=0 desc, sort asc')->group('brand_id')->field('brand_id,goods_id')->select();
+        $goods = db('goods')->group('brand_id')->field('brand_id,goods_id')->select();
         $this->assign('page',$page);
         $this->assign('goods',array_column($goods,'goods_id','brand_id'));
         $this->assign('list',$brand);
@@ -652,7 +652,7 @@ class Goods extends Base {
         $condistion = ['cat_id'=>$cat_id,'is_on_sale' => 1, 'exchange_integral' => 0,'brand_id'=>$brand_ids[$cat_id]];
         $count = db('goods')->where($condistion)->count();   //总页数
         $page = new Page($count, 4, '', 3);
-        $goods = db('goods')->where($condistion)->order('sort','asc')->limit($page->firstRow . ',' . $page->listRows)->select();
+        $goods = db('goods')->where($condistion)->order('store_count !=0 desc, sort asc')->limit($page->firstRow . ',' . $page->listRows)->select();
         $this->assign('page',$page);
         $this->assign('list',$goods);
         $this->assign('name',$name);
